6e2eae58bb564358937c7f094dc9a52f0077e369
3rd <3rd@users.noreply.github.com>
2023-06-14 18:32:40 +0300

    chore: add schema type checks

--- src/dev.ts
+++ src/dev.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import type {
   HasWildcard,
   InferSubscriptionHandler,
@@ -9,7 +9,6 @@ import type {
 import { z } from "zod";
 import { create } from ".";
 
-// debug
 const testSchema = {
   foo: z.object({ field: z.string() }),
   bar: {
@@ -17,41 +16,98 @@ const testSchema = {
   },
   zap: {
     zop: {
-      zup: {
-        zip: z.object({ field: z.string() }),
-      },
+      zup: z.object({ field: z.string() }),
     },
   },
 };
+const test = create({ schema: testSchema });
 
-type DebugSubscriptionHandlers = MappedSubscriptionHandlers<typeof testSchema>;
 type DebugSchemaPaths = SchemaPaths<typeof testSchema>;
 type DebugWildcardPaths = WildcardPaths<SchemaPaths<typeof testSchema>>;
-type DebugHasWildcard = HasWildcard<"zap.zop.*.zip">;
-type DebugInferHandler = InferSubscriptionHandler<typeof testSchema, "zap.zop.*.zip">;
+type DebugHasWildcard = HasWildcard<"zap.*.zup">;
+type DebugInferHandler = InferSubscriptionHandler<typeof testSchema, "foo.bar.*">;
+type DebugSubscriptionHandlers = MappedSubscriptionHandlers<typeof testSchema>;
 
-const test = create({ schema: testSchema });
-test.subscribe("zap.zop.*.zip", (data, event) => {
-  data; // string
-  event; // unknown
+const testSchemaPaths: "foo" | "bar.baz" | "zap.zop.zup" = {} as SchemaPaths<typeof testSchema>;
+const testWildcardPaths:
+  | "foo"
+  | "*"
+  | "bar.baz"
+  | "zap.zop.zup"
+  | "zap.*.zup"
+  | "bar.*"
+  | "*.baz"
+  | "*.*"
+  | "zap.*.*"
+  | "zap.zop.*"
+  | "zap.*"
+  | "*.zop.zup"
+  | "*.*.*"
+  | "*.zop.*"
+  | "*.*.zup" = {} as WildcardPaths<SchemaPaths<typeof testSchema>>;
+
+const testHasWildcard: true = {} as HasWildcard<"*.zop.zup">;
+const testHasWildcard2: true = {} as HasWildcard<"zap.*.zup">;
+const testHasWildcard3: true = {} as HasWildcard<"zap.zop.*">;
+const testHasNoWildcard: false = {} as HasWildcard<"z*ap.zop.zup">;
+
+const testInferFooHandler: (data: { field: string }, event: "foo") => void = {} as InferSubscriptionHandler<
+  typeof testSchema,
+  "foo"
+>;
+const testInferBarBazHandler: InferSubscriptionHandler<typeof testSchema, "bar.baz"> = (
+  data: { field: string | number },
+  event: "bar.baz"
+) => {};
+const testInferWildcardHandler: InferSubscriptionHandler<typeof testSchema, "bar.*"> = (
+  data: unknown,
+  event: string
+) => {};
+
+const testSubscriptionHandlers: {
+  foo: (data: { field: string }, event: "foo") => void;
+  "*": (data: { field: string }, event: string) => void;
+  "bar.baz": (data: { field: string | number }, event: "bar.baz") => void;
+  "bar.*": (data: { field: string | number }, event: string) => void;
+  "*.baz": (data: { field: string | number }, event: string) => void;
+  "*.*": (data: { field: string | number }, event: string) => void;
+  "zap.zop.zup": (data: { field: string }, event: "zap.zop.zup") => void;
+  "zap.zop.*": (data: { field: string }, event: string) => void;
+  "zap.*.zup": (data: { field: string }, event: string) => void;
+  "zap.*.*": (data: { field: string }, event: string) => void;
+  "*.zop.zup": (data: { field: string }, event: string) => void;
+  "*.zop.*": (data: { field: string }, event: string) => void;
+  "*.*.zup": (data: { field: string }, event: string) => void;
+  "*.*.*": (data: { field: string }, event: string) => void;
+} = {} as MappedSubscriptionHandlers<typeof testSchema>;
+
+// PASS
+test.subscribe("zap.*.zup", (data, event) => {
+  // data: unknown
+  // event: string
 });
-test.subscribe("foo", (data) => {
-  data.field; // string
+test.subscribe("foo", (data, event) => {
+  // data: { field: string }
+  // event: "foo"
 });
 test.subscribe("bar.baz", (data, event) => {
-  data.field; // string
+  // data: { field: string | number }
+  // event: "bar.baz"
 });
-test.subscribe("zap.zop.zup.zip", (data) => {
-  data.field; // string
+test.subscribe("zap.zop.zup", (data) => {
+  // data: { field: string }
+  // event: "zap.zop.zup"
 });
-// fail
-test.subscribe("zap.zop.zup.unknown", (data) => {
-  data.field; // string
-});
-// fail
-test.subscribe("zap.zop.zup.*", (data) => {
-  data.unknown; // fail
+test.publish("bar.baz", { field: "test" });
+
+// FAIL
+// @ts-expect-error
+test.subscribe("zap.zop.zup.unknown", () => {});
+
+test.subscribe("zap.zop.*", (data) => {
+  // @ts-expect-error
+  console.log(data.unknown); // fail
 });
 
-test.publish("bar.baz", { field: "test" });
+// @ts-expect-error
 test.publish("foo.*", { field: "test" });
