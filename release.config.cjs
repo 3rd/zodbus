41a2f8a1adcff130997aa9cf683698650d2781e6
3rd <3rd@users.noreply.github.com>
2023-06-15 21:05:10 +0300

    chore: setup CI testing and release

--- release.config.cjs
+++ release.config.cjs
@@ -1,11 +1,5 @@
 module.exports = {
-  branches: [
-    "master",
-    {
-      name: "beta",
-      prerelease: true,
-    },
-  ],
+  branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
